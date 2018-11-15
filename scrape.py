623478f104c2e5288cbca7bc67d72d4eff1fe394
HHassig <harrisonhassig@gmail.com>
2018-11-15 14:07:42 -0800

    cleaned data, complete

--- scrape.py
+++ scrape.py
@@ -2,13 +2,23 @@ import requests
 from bs4 import BeautifulSoup, Comment
 from prettytable import PrettyTable
 
+
+# HTML Remover
+def stripHTML(aTag):
+	return aTag.get_text()
+# Pre Work
 numberOfPlayers = -1
 teams = ["ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
-url = "https://www.basketball-reference.com/teams/" + teams[3] + "/2019.html"
+teamID = 13
+url = "https://www.basketball-reference.com/teams/" + teams[teamID] + "/2019.html"
+print(teams[teamID])
+
+# Visit page and scrape
 res = requests.get(url, headers = {"User-Agent":"Mozilla/5.0"})
 soup = BeautifulSoup(res.text, 'lxml')
 for comment in soup.find_all(string=lambda text:isinstance(text,Comment)):
 	data = BeautifulSoup(comment,"lxml")
+	# Get team stats
 	x = data.find("table", id="team_and_opponent")
 	if x != None:
 		allTeamStats = []
@@ -20,6 +30,7 @@ for comment in soup.find_all(string=lambda text:isinstance(text,Comment)):
 			for tag2 in tag.find_all("td"):
 				rows += tag2.contents
 			allTeamStats += [rows]
+	# Get player stats
 	y = data.find("table", id="per_game")
 	if y != None:
 		allPlayerStats = []
@@ -34,19 +45,8 @@ for comment in soup.find_all(string=lambda text:isinstance(text,Comment)):
 					tag2.contents = ["0.0"]
 				rows += tag2.contents
 			allPlayerStats += [rows]
-allPlayerStats.pop(0)
-playerHeader.pop(0)
-playerHeader.pop(0)
-for i in range(0, numberOfPlayers):
-	playerHeader.pop()
-playerHeader.insert(0, "Name")
-player = PrettyTable()
-player.field_names = playerHeader
-for i in range(len(allPlayerStats)):
-	player.add_row(allPlayerStats[i])
-print(player)
 
-
+# Finalize and clean data
 teamHeader.pop(0)
 for i in range(0, 8):
 	teamHeader.pop()
@@ -55,3 +55,21 @@ team.field_names = teamHeader
 team.add_row(allTeamStats[1])
 team.add_row(allTeamStats[5])
 print(team)
+
+allPlayerStats.pop(0)
+playerHeader.pop(0)
+playerHeader.pop(0)
+for i in range(0, numberOfPlayers):
+	playerHeader.pop()
+playerHeader.insert(0, "Name")
+# Clean HTML from players
+for i in range(len(allPlayerStats)):
+	allPlayerStats[i][0] = stripHTML(allPlayerStats[i][0])
+	allPlayerStats[i][2] = stripHTML(allPlayerStats[i][2])
+
+player = PrettyTable()
+player.field_names = playerHeader
+for i in range(len(allPlayerStats)):
+	player.add_row(allPlayerStats[i])
+print(player)
+
